f44ecf8790d78032eb07ed7da70fdc82811d58aa
Jagdish Pal <Jagdish Pal>
2022-11-17 11:10:04 +0530

    title updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      Imprimis Will Live Soon
+      Magic is About to happen.....
       </header>
     </div>
   );
